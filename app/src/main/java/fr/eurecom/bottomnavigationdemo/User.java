47b27f23e82d2a9175d288f74a7241d8830ddce0
martisba <martinschbakken@gmail.com>
2022-01-06 16:27:50 +0100

    Created user class

---
+++ app/src/main/java/fr/eurecom/bottomnavigationdemo/User.java
@@ -0,0 +1,88 @@
+package fr.eurecom.bottomnavigationdemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class User {
+
+    private String UID;
+    private String name;
+    private String age;
+    private String gender;
+    private String phone;
+
+    private Location location;
+    private LocationManager locationManager;
+
+
+    public User(String UID, String name, String age, String gender, String phone) {
+        setUID(UID);
+        setName(name);
+        setAge(age);
+        setGender(gender);
+        setPhone(phone);
+        //setLocation);
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+
+
+
+
+}

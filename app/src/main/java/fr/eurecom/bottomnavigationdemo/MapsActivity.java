80106213656a5656faa59784005a37d3703eef41
Carl-Johan Björnson <calle.bjornson@gmail.com>
2022-01-06 15:24:24 +0100

    test for new branch

--- app/src/main/java/fr/eurecom/bottomnavigationdemo/MapsActivity.java
+++ app/src/main/java/fr/eurecom/bottomnavigationdemo/MapsActivity.java
@@ -139,6 +139,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onSaveInstanceState(outState);
     }
     // ################################
+    // Test comment
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
